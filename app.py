e9799b23336ffe803a35dcc01f74966aaf0c55e9
Juan Carlos Banos <juancarlosbanosmafud@gmail.com>
2022-11-15 20:05:05 -0600

    final version of streamlit website

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.write('Welcome to my app')
+
+@st.cache
+def get_line_chart_data():
+
+    return pd.DataFrame(
+            np.random.randn(20, 3),
+            columns=['a', 'b', 'c']
+        )
+
+df = get_line_chart_data()
+
+st.line_chart(df)
